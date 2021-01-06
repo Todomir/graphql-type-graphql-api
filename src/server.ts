6000e16cf42e323bbdf915a86b9cf2de9ea35f3b
Abner Rodrigues <abnerluis1001@gmail.com>
2021-01-06 19:59:20 -0300

    build: changes to tsconfig

--- src/server.ts
+++ src/server.ts
@@ -11,12 +11,14 @@ import './database/schemas/UserSchema'
 
 import UserResolver from './resolvers/UserResolver'
 
+const port: string | number = process.env.PORT || 4000
+
 async function app() {
   const schema = await buildSchema({ resolvers: [UserResolver] })
   const server = new ApolloServer({ schema })
 
-  server.listen({ port: process.env.PORT || 4000 }, () => {
-    console.log(`⚡️[server]: Server is running on port ${process.env.PORT}`)
+  server.listen({ port: port }, () => {
+    console.log(`⚡️[server]: Server is running on port ${port}`)
   })
 }
 
